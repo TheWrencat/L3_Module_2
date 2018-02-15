48973960e8e8c508023f90b827ce48e121c6106c
TheWrencat <league@WTS14.attlocal.net>
2018-02-14 19:33:30 -0800

    finished first file assignment

--- src/encryption.java
+++ src/encryption.java
@@ -1,8 +1,28 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 import javax.swing.JOptionPane;
 
 public class encryption {
 	public static void main(String[] args) {
 		String message = JOptionPane.showInputDialog("Input message to encrypt.");
+		// put encryption here
+		String scrambled = "";
+		for (int i = 0; i < message.length(); i++) {
+			char c = message.charAt(i);
+			scrambled += (char) (c + 5);
+		}
+
+		try {
+			FileWriter file = new FileWriter("src/intro_to_file_io/encryptedText.txt");
+			file.write("\n" + scrambled);
+			file.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 }
