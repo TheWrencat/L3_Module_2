1fcba38112be6c8fd8d9d0554ecb422ed4b5031e
GeneralOy <League@AdministorsiMac.attlocal.net>
2018-02-22 20:01:37 -0800

    Yeah

--- src/intro_to_file_io/toDoList.java
+++ src/intro_to_file_io/toDoList.java
@@ -1,5 +1,43 @@
 package intro_to_file_io;
 
-public class toDoList {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class toDoList implements ActionListener {
+	JFrame frame;
+	JPanel panel;
+	JButton addTask;
+	JButton removeTask;
+	JButton save;
+	JButton load;
+	
+	void setup(){
+		frame = new JFrame();
+		panel = new JPanel();
+		addTask = new JButton();
+		removeTask = new JButton();
+		save = new JButton();
+		load = new JButton();
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(addTask);
+		panel.add(removeTask);
+		panel.add(save);
+		panel.add(load);
+		addTask.addActionListener(this);
+		removeTask.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
