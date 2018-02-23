ba51498664b06afdfb24950cb401736404476e69
GeneralOy <League@AdministorsiMac.attlocal.net>
2018-02-22 19:46:52 -0800

    Copyright in progres

--- src/intro_to_file_io/Copyright.java
+++ src/intro_to_file_io/Copyright.java
@@ -1,5 +1,13 @@
 package intro_to_file_io;
 
+import java.io.File;
+import java.io.FileWriter;
+
 public class Copyright {
 
+	
+	public static void main(String[] args) {
+		File file = new File("src/intro_to_file_io/");
+	}
+	
 }
